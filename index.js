9da93f0772fdb033f5a913f7bba37bc532fe48d6
NHQ SERVER ADMIN <nhq@NHQ.att.net>
2014-03-22 10:01:00 -0700

    fixed bugs and made it all right

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var http = require('http')
 ,   path = require('path')
 ,   net = require('net')
 ,   orgv = require('optimist').argv
+,   vargs = require('minimist')(process.argv.slice(2))
 ,   fs = require('fs')
 ,   spawn = require('child_process').spawn
 ,   openSesame = require('sesame-stream')
@@ -17,12 +18,13 @@ var http = require('http')
 ,   dir
 ;
 
+console.log(vargs, process.argv)
 if(orgv.c){
-	fs.mkdirSync(path.resolve('./', orgv.c))
-	fs.writeFileSync(path.resolve('./', orgv.c) + '/index.js')
-	fs.writeFileSync(path.resolve('./', orgv.c) + '/entry.js')
-	spawn('cp', ['-R', __dirname + '/public', path.resolve('./', orgv.c)])
-	return
+  fs.mkdirSync(path.resolve('./', orgv.c))
+  fs.writeFileSync(path.resolve('./', orgv.c) + '/index.js')
+  fs.writeFileSync(path.resolve('./', orgv.c) + '/entry.js')
+  spawn('cp', ['-R', __dirname + '/public', path.resolve('./', orgv.c)])
+  return
 }
 
 if(process.argv[2] && process.argv[2].charAt(0).match(/[A-Za-z0-9]+/g)){
@@ -74,22 +76,22 @@ if(orgv.d) oargs.push('-d')
 
 app = path.resolve(cwd, app)
 console.log(app, output)
-var b = spawn('watchify', oargs)
+var b = spawn('watchify', process.argv.slice(2)) //oargs)
 
 b.stderr.on('data', function(data){ console.log(data.toString('utf8'))});
 
 var server = http.createServer(function(req, res){
 
     if(req.url === '/' + output){
-		fs.createReadStream(dir + '/' + output).pipe(res)
+    fs.createReadStream(dir + '/' + output).pipe(res)
     }
 
 /*
     if(req.url === '/' + output){
-	res.writeHead(200, {'Content-Type': 'text/javascript'});
-	var b = spawn(command, ['-e', app, '-t', 'brfs', '-d']);
-	b.stdout.pipe(res);
-	b.stderr.on('data', function(data){ console.log(data.toString('utf8'))});
+  res.writeHead(200, {'Content-Type': 'text/javascript'});
+  var b = spawn(command, ['-e', app, '-t', 'brfs', '-d']);
+  b.stdout.pipe(res);
+  b.stderr.on('data', function(data){ console.log(data.toString('utf8'))});
     }
 */
 
@@ -104,24 +106,37 @@ var server = http.createServer(function(req, res){
 
 server.on('upgrade', openSesame)
 
-server.listen(port);
+listen(server, 11001)
+//server.listen(port);
 
 server.on('error', function(e){
-	if (e.code == 'EADDRINUSE') {
-		console.log('port ' + port + ' occupied. Use -p to set another port.')
-		process.exit();
-	}
-});
-
-server.on('listening', function(){
-
-  if(!orgv.n) console.log('Server listening at http://localhost:' + port)
-
-	else console.log('An empty html page with your module is being served at http://localhost:' + port)
-
+  if (e.code == 'EADDRINUSE') {
+    console.log('port ' + port + ' occupied. Use -p to set another port.')
+    process.exit();
+  }
 });
 
 
+function listen(server, _port){
+  isPortTaken(_port, answer)
+  function answer(err, b){
+    if(!b) {
+      server.listen(_port)
+      
+      server.on('listening', function(){
+
+        if(!orgv.n) console.log('Server listening at http://localhost:' + _port)
+
+        else console.log('An empty html page with your module is being served at http://localhost:' + _port)
+
+      });
+    }
+    else {
+      port = _port
+      isPortTaken(++_port, answer)
+    }
+  }
+}
 function isPortTaken(PORT, callback) {
   var tester = net.createServer()
   tester.once('error', function (err) {
